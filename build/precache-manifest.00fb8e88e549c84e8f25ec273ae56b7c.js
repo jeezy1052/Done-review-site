fd5f375469b80517256f0c1b3a593b2f31e0f103
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-02-10 21:21:53 -0800

    i just finshed site for client

---
+++ build/precache-manifest.00fb8e88e549c84e8f25ec273ae56b7c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda06f0ad68f7105bba755126ddb659c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25f243c51659ffb4ff98",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "959897a691e512b6bed9",
+    "url": "/static/js/2.7145c435.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.7145c435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f243c51659ffb4ff98",
+    "url": "/static/js/main.9f26f7b9.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "deebf34b57267aeadcf49a8e680b7ffd",
+    "url": "/static/media/2.deebf34b.jpg"
+  },
+  {
+    "revision": "249c330c748c814f7952455b4f168e15",
+    "url": "/static/media/Slice 1med.249c330c.png"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  }
+]);
